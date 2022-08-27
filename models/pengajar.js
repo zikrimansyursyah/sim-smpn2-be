434b6b1a5bc55439faa8fb7840120e8bbeec521f
Aqidatul Izzah Ch <aqidatulizzahchairul@gmail.com>
2022-08-27 21:06:30 +0700

    model pengajar

---
+++ models/pengajar.js
@@ -0,0 +1,31 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class pengajar extends Model {
+    static associate(models) {
+    }
+  }
+  pengajar.init(
+    {
+      id_pengajar: {
+        type: DataTypes.INTEGER,
+      },
+      id_kelas: {
+        type: DataTypes.INTEGER,
+      },
+      id_mapel: {
+        type: DataTypes.INTEGER,
+      },
+      createdBy: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      updatedBy: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "pengajar",
+    }
+  );
+  return pengajar;
+};
